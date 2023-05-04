0698a7a6adac8c5c3dc5d5d75a20d41d92c7f70a
Paulo Ferreira <pauloricardofr@hotmail.com>
2023-05-04 10:41:48 -0300

    alteração no programa

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String agencia, nome;
+        int numero;
+        double saldo;
+
+        Scanner sc = new Scanner(System.in);
+        {
+
+            System.out.println("Por favor, digite o número da Agência!");
+            agencia = sc.nextLine();
+
+            System.out.println("Por favor, digite o número da Conta!");
+            numero = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Por favor, digite o Nome!");
+            nome = sc.nextLine();
+
+            System.out.println("Digite o saldo!");
+            saldo = sc.nextDouble();
+
+            System.out.printf("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                    + agencia + ", conta " + numero + " e seu saldo " + saldo + " está disponível para saque.");
+
+        }
     }
 }
